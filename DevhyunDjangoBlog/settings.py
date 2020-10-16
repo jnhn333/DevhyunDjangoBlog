bb70a1c53cc78c4e1f7ee2e272b6f57998c63b3d
KPIC-PJHPC <d3vhyun@gmail.com>
2020-10-16 15:12:19 +0900

    First Commit

--- DevhyunDjangoBlog/settings.py
+++ DevhyunDjangoBlog/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-with open("C:/django/DevhyunDjango/secret.json") as f:
+with open("secret.json") as f:
     secrets = json.loads(f.read())
 def get_secret(setting, secrets=secrets):
     try :
