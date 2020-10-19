6596d27d838d3bc7c46cfdd56615c0feb22df9ad
KPIC-PJHPC <d3vhyun@gmail.com>
2020-10-19 18:13:41 +0900

    page link, CSS Change

--- Blog/views.py
+++ Blog/views.py
@@ -6,9 +6,8 @@ from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-# return render(request, '/home/devhyun/DevhyunDjangoBlog/Blog/templates/Blog/post_list.html', {'posts': posts})
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'Blog/post_detail.html', {'post': post})
