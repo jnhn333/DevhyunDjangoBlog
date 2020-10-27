68235e92940ea09fc5af3f0a4b59406c5d73b25a
KPIC-PJHPC <d3vhyun@gmail.com>
2020-10-27 19:37:19 +0900

    develop Blog!

--- Blog/views.py
+++ Blog/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.http import HttpResponse
+from django.contrib.auth import get_user_model
 # Create your views here.
 
 def post_list(request):
@@ -19,8 +21,8 @@ def post_new(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
+            #post.author = request.user
+            post.author = get_user_model().objects.get(username='admin')
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -33,12 +35,26 @@ def post_edit(request, pk):
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
-            post.author = request.user
-            post.published_date = timezone.now()
+            #post.author = request.user
+            post.author = get_user_model().objects.get(username='admin')
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'Blog/post_edit.html', {'form': form})
 
-def admin(request): return ("")
+def admin(request): return HttpResponse(admin)
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'Blog/post_draft_list.html', {'posts': posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
